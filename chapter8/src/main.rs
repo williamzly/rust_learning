d93801e0bf879e7e26e1e362d19990e22d0a1613
william <4269946@qq.com>
2021-11-16 00:20:25 +0800

    chapter 8 -- pig_latin

--- chapter8/src/main.rs
+++ chapter8/src/main.rs
@@ -49,6 +49,39 @@ fn mode_of (list: &Vec<usize>) -> Vec<usize> {
 
 // Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
 
+fn pig_latin(word: &str) -> String {
+    if word.is_empty() {
+        return String::new();
+    }
+    let vowel :[char; 5] = ['a', 'o', 'e', 'i', 'u'];
+    let s = String::from(word);
+    let chars = s.char_indices();
+    let mut new_chars: Vec<char> = Vec::new();
+    let mut is_start_with_vowel = false;
+    let mut first_char: char = '0';
+    for (i,  c) in chars {
+        if i == 0 {
+            if vowel.contains(&c) {
+                is_start_with_vowel = true;
+                new_chars.push(c);
+            }
+            first_char = c;
+        } else {
+            new_chars.push(c);
+        }
+    }
+    let suffix = if is_start_with_vowel {
+        String::from("-hay")
+    } else {
+        "-" .to_owned()+ &first_char.to_string() + "ay"
+    };
+    let mut result = String::new();
+    for c in new_chars {
+        result.push(c);
+    }
+    result + &suffix
+}
+
 
 // Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
 
@@ -60,4 +93,9 @@ fn main() {
     println!("It's average: {}", average_of(&a_list));
     println!("It's median: {}", median_of(&a_list));
     println!("It's mode: {:?}", mode_of(&a_list));
+
+    println!("Apple's Pig Latin is {}", pig_latin("apple"));
+    println!("World's Pig Latin is {}", pig_latin("world"));
+    println!("empty Pig Latin is {}", pig_latin(""));
+    println!("H's Pig Latin is {}", pig_latin("h"));
 }
