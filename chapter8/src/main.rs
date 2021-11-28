f8275c3407aaae815c3430160c1128d87def2fdf
william <4269946@qq.com>
2021-11-28 21:27:35 +0800

    complete charpter8

--- chapter8/src/main.rs
+++ chapter8/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::io;
 
 // Given a list of integers, use a vector and return the mean (the average value), median (when sorted, the value in the middle position), and mode (the value that occurs most often; a hash map will be helpful here) of the list.
 fn average_of (list: &Vec<usize>) -> f32 {
@@ -85,6 +86,130 @@ fn pig_latin(word: &str) -> String {
 
 // Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
 
+fn parse_user_input_for_adding_emploee(input: &str) -> Option<(&str, &str)> {
+    let mut words = input.trim().split_whitespace();
+    let maybe_add = words.next();
+    if maybe_add.is_none()|| !maybe_add.expect("").eq_ignore_ascii_case("add") {
+       return None;
+    }
+    let maybe_employee = words.next();
+    if maybe_employee.is_none() {
+        return None;
+    }
+    let maybe_to = words.next();
+    if maybe_to.is_none()|| !maybe_to.expect("").eq_ignore_ascii_case("to") {
+        return None;
+    }
+    let maybe_department = words.next();
+    if maybe_department.is_none() {
+        return None;
+    } else {
+        return Some((maybe_employee.expect(""), maybe_department.expect("")));
+    }
+}
+
+fn employee_management_system_text_interface() {
+    let mut company = Company::new();
+    loop {
+        let mut input = String::new();
+        println!("Please input \n1 for add new employee \n2 for retrieve all people in a department \n3 for retrieve all people in company by department");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        match input.trim().parse() {
+            Ok(i) => match i {
+                1 => {
+                    company.add_new_employee();
+                    continue;
+                },
+                2 => {
+                    company.retrieve_by_department();
+                    continue;
+                },
+                3 => {
+                    company.retrieve_all_people();
+                    break;
+                },
+                _ => {
+                    println!("nothing was matched");
+                    continue;
+                },
+            },
+            Err(_) => {
+                println!("Err while parse input to u32");
+                continue;
+            }
+        };
+    }
+}
+
+struct Company {
+    structure: HashMap<String, Vec<String>>
+}
+
+impl Company {
+    fn new() -> Company {
+        Company {
+            structure: HashMap::new()
+        }
+    }
+    fn add_new_employee(&mut self) {
+        println!("Please input department and employee name as \"department employee\" (no quotes)");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let mut input = input.split_whitespace();
+        let department = input.next().expect("");
+        let employee = input.next().expect("");
+        self.add_new_employee_internal(&department, &employee);
+        println!("Added {} to {} success!", employee, department);
+    }
+    fn add_new_employee_internal(&mut self, department: &str, employee: &str) {
+        match self.structure.get_mut(department) {
+            Some(employees) => employees.push(employee.to_string()),
+            None => {
+                let mut employees = Vec::new();
+                employees.push(String::from(employee));
+                self.structure.insert(department.to_string(), employees);
+            }
+        }
+    }
+    fn retrieve_by_department(&mut self) {
+        println!("Please input department name");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let maybe_employees = self.retrieve_by_department_internal(&input.trim());
+        if maybe_employees.is_none() {
+            println!("No employee in {}", input);
+        } else {
+            println!("---LIST---");
+            for e in maybe_employees.expect("No employee in this department!!").iter() {
+                println!("{}", e);
+            }
+            println!("----------");
+        }
+    }
+    fn retrieve_by_department_internal(&mut self, department: &str) -> Option<&Vec<String>> {
+        self.structure.get(department)
+    }
+    fn retrieve_all_people(&self) {
+        let mut sorted = Vec::new();
+        for employees in self.structure.values() {
+            for e in employees {
+                sorted.push(e);
+            }
+        }
+        sorted.sort();
+        println!("---LIST---");
+        for e in sorted.iter() {
+            println!("{}", e);
+        }
+        println!("----------");
+    }
+}
 
 
 fn main() {
@@ -97,5 +222,9 @@ fn main() {
     println!("Apple's Pig Latin is {}", pig_latin("apple"));
     println!("World's Pig Latin is {}", pig_latin("world"));
     println!("empty Pig Latin is {}", pig_latin(""));
-    println!("H's Pig Latin is {}", pig_latin("h"));
+    println!("H's Pig Latin is {}", pig_latin("H"));
+
+    
+    println!("{:?}", parse_user_input_for_adding_emploee("aDd 123 to 321"));
+    employee_management_system_text_interface();
 }
