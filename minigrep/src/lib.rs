bda63d0462dba268a916b4975c611d2cef0ce62d
william <4269946@qq.com>
2021-12-11 16:35:17 +0800

    implement search feature with a test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -6,9 +6,22 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let content = fs::read_to_string(&config.filename)?;
 
-    println!("Content: \r\n{}", content);
+    for line in search(&config.query, &content) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let lines = content.lines();
+    let mut result = Vec::new();
+    for line in lines {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
 }
 
 pub struct Config {
@@ -27,4 +40,23 @@ impl Config {
             filename: args[2].clone()
         })
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust
+safe, fast, productive.
+Pick three.";
+        assert_eq!(
+          vec!("safe, fast, productive."),
+          search(query, content)
+        );
+    }
+
 }
