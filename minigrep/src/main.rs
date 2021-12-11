007ab48a115b42566ecd4385b52baa72e68fc515
william <4269946@qq.com>
2021-12-11 09:29:29 +0800

    print search and filename from input args

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,6 +3,10 @@ use std::env;
 fn main() {
     
     let args :Vec<String> = env::args().collect();
-    println!("args: {:?}", args)
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("search {} from {}", query, filename);
 
 }
