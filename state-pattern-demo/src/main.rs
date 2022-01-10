ae731b285d9066cf1e262edf467f066a50179d12
william <4269946@qq.com>
2022-01-10 13:06:50 +0800

    follow document

---
+++ state-pattern-demo/src/main.rs
@@ -0,0 +1,102 @@
+
+struct Post {
+    state: Option<Box<dyn State>>,
+    content: String
+}
+
+impl Post {
+
+    pub fn new() -> Post {
+        Post {
+            state: Some(
+                Box::new(
+                    Draft {}
+                )
+            ),
+            content: String::new()
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(&self)
+    }
+
+    pub fn add_content(&mut self, content: &str) {
+        self.content.push_str(content);
+    }
+
+    pub fn request_review(&mut self) {
+        self.state = Some(
+            self.state.take().unwrap().request_review()
+        );
+    }
+
+    pub fn approve(&mut self) {
+        self.state = Some(
+            self.state.take().unwrap().approve()
+        );
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {
+
+}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {
+
+}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {
+
+}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
+
+
+
+fn main() {
+    let mut post = Post::new();
+    assert_eq!(post.content(), "");
+    post.add_content("Some text...");
+    assert_eq!(post.content(), "");
+    post.request_review();
+    assert_eq!(post.content(), "");
+    post.approve();
+    assert_eq!(post.content(), "Some text...");
+}
